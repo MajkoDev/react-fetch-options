071be2d356de6ca2b82f13f6094fb2ee7d2620d2
Marian <hellomariandev@gmail.com>
2023-01-29 13:53:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+// Components
+import {
+  FetchData,
+  FetchDataAsync,
+  FetchHook,
+  FetchAxios,
+  FetchSwr,
+} from "../components";
+
 function App() {
   return (
     <div className="h-screen bg-slate-50 flex-cen">
-      <h1 className="title">Vite React Tailwind</h1>
+      <h1 className="title m-2">Vite React Tailwind</h1>
+
+      <div className="flex-cen container mb-4">
+        <FetchData />
+        <FetchDataAsync />
+      </div>
+      <div className="flex-cen container mb-4">
+        <FetchHook />
+        <FetchAxios />
+        <FetchSwr />
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/*.{js,jsx}",
+    "./components/*.{js,jsx}"
   ],
   theme: {
     extend: {},
