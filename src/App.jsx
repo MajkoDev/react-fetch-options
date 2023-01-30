632163ed64d003a15c46ee04717cd240b31094ae
Marian <hellomariandev@gmail.com>
2023-01-30 22:59:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // Components
+import { useState } from "react";
 import {
   FetchData,
   FetchDataAsync,
@@ -8,21 +9,52 @@ import {
 } from "../components";
 
 function App() {
-  return (
-    <div className="min-h-screen flex-cen">
-      <h1 className="title m-2">Vite React Tailwind</h1>
+  const [option, setOption] = useState(false);
+  const [library, setLibrary] = useState(false);
 
-      <div className="flex-cen container mb-4">
-        <FetchData />
+  const chooseOption = () => {
+    setOption(!option);
+    setLibrary(false);
+  };
+
+  const showLibraries = () => {
+    setLibrary(!library);
+    setOption(false);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col justify-start items-center">
+      <h1 className="title m-2">Vite React Tailwind</h1>
+      <div className="bg-slate-100 w-screen mt-2 mb-6 flex justify-center">
+        <button
+          onClick={() => chooseOption()}
+          className="px-2 py-1 my-3 mx-2 uppercase hover:bg-slate-400 rounded-lg text-md font-medium"
+        >
+          fetch
+        </button>
+        <button
+          onClick={() => showLibraries()}
+          className="px-2 py-1 my-3 mx-2 uppercase hover:bg-slate-400 rounded-lg text-md font-medium"
+        >
+          library
+        </button>
       </div>
-      {/*
-        <FetchDataAsync />
-        <FetchHook />
-        <FetchAxios />
-        <FetchSwr />
-      */}
-      <div className="flex-cen container mb-4">
-      </div>
+
+      {option && (
+        <>
+          <FetchData />
+          <FetchDataAsync />
+        </>
+      )}
+
+      {library && (
+        <>
+          <FetchHook />
+          <FetchAxios />
+          <FetchSwr />
+        </>
+      )}
+
     </div>
   );
 }
