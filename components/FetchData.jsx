e20bb1a1d252e323bcca81e599cef8a99ae96d02
Marian <hellomariandev@gmail.com>
2023-01-30 23:43:30 +0100

    fetch() method with loading and error

--- components/FetchData.jsx
+++ components/FetchData.jsx
@@ -1,15 +1,38 @@
 //TODO:     Fetching API using fetch() method
 
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const FetchData = () => {
-  return (
-    <div>
-      <h2 className="subtitle">
-        I. Fetching API using fetch()
-      </h2>
-    </div>
-  )
-}
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default FetchData
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw res;
+      })
+      .then((data) => setUsers(data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+
+  return (
+    <div className="basis-1/3">
+      <h2 className="subtitle">Fetching API using fetch()</h2>
+      {users.map((user) => (
+        <li key={user.id} className="text-sm list-none m-1">
+          {user.name}
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default FetchData;
