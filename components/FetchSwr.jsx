a1379e33d6a957279213ec6f089981b35e13d295
Marian <hellomariandev@gmail.com>
2023-01-31 23:33:21 +0100

    fetching data with Swr library

--- components/FetchSwr.jsx
+++ components/FetchSwr.jsx
@@ -1,11 +1,29 @@
 //TODO:     Fetching API using third library: SWR
 
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const FetchSwr = () => {
+  
+  const { data, isLoading, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
-    <div>
-      <h2 className="subtitle">V. Fetching API using third library: SWR</h2>
+    <div className="basis-1/3">
+      <h2 className="subtitle">fetching API using SWR</h2>
+
+      {data.map(({ id, email }) => (
+        <li className="text-sm font-medium list-none m-1" key={id}>
+          <p>{email}</p>
+        </li>
+      ))}
     </div>
   );
 };
