0282e1f3bfc5a3f8417b094638ddef2119877eb9
Marian <hellomariandev@gmail.com>
2023-01-31 23:19:23 +0100

    fetching data using Axios library

--- components/FetchAxios.jsx
+++ components/FetchAxios.jsx
@@ -1,11 +1,25 @@
 //TODO:     Fetching API using third library: Axios
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const FetchAxios = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
   return (
-    <div>
-      <h2 className="subtitle">IV. Fetching API using third library: Axios</h2>
+    <div className="basis-1/3">
+      <h2 className="subtitle">fetching API using Axios</h2>
+      {users.map((user) => (
+        <li key={user.id} className="text-sm list-none m-1">
+          {user.username}
+        </li>
+      ))}
     </div>
   );
 };
