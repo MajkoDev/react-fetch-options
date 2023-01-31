c17be97c6c6614ee9af7f43309e5e9f7798fd1eb
Marian <hellomariandev@gmail.com>
2023-01-31 23:19:33 +0100

    fetching data with custom hook

--- components/FetchHook.jsx
+++ components/FetchHook.jsx
@@ -1,13 +1,24 @@
 //TODO:     Fetching API using custom hook: react-fetch-hook
 
 import React from "react";
+import useFetch from "react-fetch-hook";
 
 const FetchHook = () => {
+  const { data, isLoading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
   return (
-    <div>
-      <h2 className="subtitle">
-        III. Fetching API using custom hook: react-fetch-hook
-      </h2>
+    <div className="basis-1/3">
+      <h2 className="subtitle">fetching API using custom hook</h2>
+      {isLoading && <h2>Wait a minute...</h2>}
+      {error && <h3>There is a problem somewhere!</h3>}
+      {data &&
+        data.map(({ id, name }) => (
+          <p className="text-sm italic" key={id}>
+            <h3>{name}</h3>
+          </p>
+        ))}
     </div>
   );
 };
